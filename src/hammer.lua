c651e28be42e1d259c664e07b49a2f2e0ecd45ac
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-09-16 20:49:15 -0400

    Abstracted out player facing axis checks

--- src/hammer.lua
+++ src/hammer.lua
@@ -23,9 +23,7 @@
 -- TODO set up registering system for hammers.
 -- TODO add hammers for all metal and gem materials.
 -- TODO add crafting recipes.
--- TODO make group checking use groupcaps present in item.
 -- TODO add division (like 1/3) to the total adjusted mining time of the hammers to make them a little faster.
--- TODO factor out common code.
 
 -- Imports private namespace.
 local _gigatools = ...
@@ -93,6 +91,22 @@ local function is_meant_to_break(toolitem, node)
    return false
 end
 
+local half_pi    = math.pi / 2
+local quarter_pi = math.pi / 4
+-- How many radians the player's pitch can be from +/- pi/2 (looking straight up
+-- or down) to count as facing vertically.
+local vertical_mining_epsilon_rad = 0.2
+
+local function is_player_facing_y_axis(player)
+   return math.abs(player:get_look_vertical()) > half_pi - vertical_mining_epsilon_rad
+end
+
+local function is_player_facing_x_axis(player)
+   local yaw_rad = player:get_look_horizontal()
+   return (yaw_rad > quarter_pi and yaw_rad < math.pi - quarter_pi)
+       or (yaw_rad > 3*half_pi - quarter_pi and yaw_rad < 2*math.pi - quarter_pi)
+end
+
 
 
 -- TODO make use digger:get_wielded_item().
@@ -111,12 +125,6 @@ local function break_3x3_plane(position, axis1_field, axis2_field, digger, tooli
    end, position, axis1_field, axis2_field)
 end
 
-local half_pi    = math.pi / 2
-local quarter_pi = math.pi / 4
--- How many radians the player's pitch can be from +/- pi/2 (looking straight up
--- or down) to count as mining vertically.
-local vertical_mining_epsilon_rad = 0.2
-
 -- Used to check if a player has hammered a node and that the resulting calls
 -- from register_on_nodedig() are the result of the hammer's 3x3 mining. This is
 -- to prevent recursively mining nodes.
@@ -134,25 +142,13 @@ local function try_hammer(position, old_node, digger)
    if not is_meant_to_break(wielded_item, old_node) then return end
 
    is_hammering[player_name] = true
-
-   -- Since we don't know which face of the node the player broke, we will have
-   -- to guess based on their rotation.
-   local yaw_rad   = digger:get_look_horizontal()
-   local pitch_rad = digger:get_look_vertical()
-
-   if math.abs(pitch_rad) > half_pi - vertical_mining_epsilon_rad then
-      -- If facing up/down "enough," break across the XZ plane.
+   if is_player_facing_y_axis(digger) then
       break_3x3_plane(position, "x", "z", digger, wielded_item)
-   elseif (yaw_rad > quarter_pi and yaw_rad < math.pi - quarter_pi)
-       or (yaw_rad > 3*half_pi - quarter_pi and yaw_rad < 2*math.pi - quarter_pi)
-   then
-      -- If facing the +/- X axis "enough," break across the ZY plane.
+   elseif is_player_facing_x_axis(digger) then
       break_3x3_plane(position, "z", "y", digger, wielded_item)
    else
-      -- If facing the +/- Z axis "enough," break across the XY plane.
       break_3x3_plane(position, "x", "y", digger, wielded_item)
    end
-
    is_hammering[player_name] = nil
 end
 minetest.register_on_dignode(try_hammer)
@@ -167,6 +163,8 @@ minetest.register_on_dignode(try_hammer)
 -- @param axis1_field The name of the field that represents the second axis (i.e. "z".)
 -- @param digger The ObjectRef thats punching the node. May be nil.
 -- @param toolitem The ItemStack to use to punch the node.
+-- @return The dig time, or 0.0, if none of the blocks are meant to be broken by
+-- the tool.
 local function get_3x3_plane_dig_time(position, axis1_field, axis2_field, puncher, toolitem)
    local dig_time = 0.0
 
@@ -196,22 +194,12 @@ local function try_adjust_hammer_dig_time(position, node, puncher, pointed_thing
    -- Wipe previous dig time adjustments.
    wielded_item:get_meta():set_tool_capabilities(nil)
 
-   -- Since we don't know which face of the node the player punched, we will
-   -- have to guess based on their rotation.
-   local yaw_rad   = puncher:get_look_horizontal()
-   local pitch_rad = puncher:get_look_vertical()
-
    local dig_time = nil
-   if math.abs(pitch_rad) > half_pi - vertical_mining_epsilon_rad then
-      -- If facing up/down "enough," get dig time of the XZ plane.
+   if is_player_facing_y_axis(puncher) then
       dig_time = get_3x3_plane_dig_time(position, "x", "z", puncher, wielded_item)
-   elseif (yaw_rad > quarter_pi and yaw_rad < math.pi - quarter_pi)
-       or (yaw_rad > 3*half_pi - quarter_pi and yaw_rad < 2*math.pi - quarter_pi)
-   then
-      -- If facing the +/- X axis "enough," get dig time of the ZY plane.
+   elseif is_player_facing_x_axis(puncher) then
       dig_time = get_3x3_plane_dig_time(position, "z", "y", puncher, wielded_item)
    else
-      -- If facing the +/- Z axis "enough," get dig time of the XY plane.
       dig_time = get_3x3_plane_dig_time(position, "x", "y", puncher, wielded_item)
    end
 
